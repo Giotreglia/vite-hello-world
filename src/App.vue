302212316c0f772d1b16199a3041669bf4f745e2
Giotreglia <giovanni.treglia@gmail.com>
2023-03-24 14:25:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+</style>
